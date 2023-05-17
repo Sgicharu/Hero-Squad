4c8970f726871e10c3d2eb4feadde8b5c0eef395
Susan Gicharu <SGicharu@safaricom.co.ke>
2023-05-17 13:19:27 +0300

    updated app.java

--- src/main/app.java
+++ src/main/app.java
@@ -1,2 +1,27 @@
 public class app {
 }
+import models.Hero;
+import models.Squad;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args){
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        }else {
+            port = 4567;
+        }
+        port(port);
