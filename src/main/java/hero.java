c9755dd528e54836d09cc4c5cfcf5338c4905daa
Susan Gicharu <SGicharu@safaricom.co.ke>
2023-05-17 13:43:36 +0300

    updated hero.java

--- src/main/java/hero.java
+++ src/main/java/hero.java
@@ -1,2 +1,13 @@
+package models;
+import javax.swing.text.html.HTMLWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class hero {
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private int id;
+    private static ArrayList<hero> instances = new ArrayList<>();
 }
