07756b139f3e6cf6f6573f816d2c18d412cf93cd
Susan Gicharu <SGicharu@safaricom.co.ke>
2023-05-17 15:07:02 +0300

    modified all

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -2,18 +2,15 @@ package models;
 import javax.swing.text.html.HTMLWriter;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-
-import java.util.ArrayList;
-
-public class hero {
+public class Hero {
     private String name;
     private int age;
     private String power;
     private String weakness;
     private int id;
-    private static ArrayList<hero> instances = new ArrayList<>();
+    private static ArrayList<Hero> instances = new ArrayList<>();
 
-    public hero(String name, Integer age, String power, String weakness) {
+    public Hero(String name, Integer age, String power, String weakness) {
         this.name = name;
         this.age = age;
         this.power = power;
@@ -29,19 +26,18 @@ public class hero {
     public int getAge() {return this.age;}
     public String getPower() {return this.power;}
     public String getWeakness() {return this.weakness;}
-    public static ArrayList<hero> getAllInstances() {return instances;}
+    public static ArrayList<Hero> getAllInstances() {return instances;}
     public static void clearAllHeroes(){instances.clear();}
     public int getId(){return id;}
-    public static hero findById(int id) {return instances.get(id-1);}
-    public static hero setUpNewHero(){
-        return new hero("Arnold",23,"flying","fire");
-    }
-    public static hero setUpNewHero1(){
-        return new hero("Janet",30,"Night Vision","Light");
-    }
-    public static hero setUpNewHero2(){
-        return new hero("Jay",30,"Night Vision","Light");
-    }
+    public static Hero findById(int id) {return instances.get(id-1);}
 
-}
+    public static Hero setUpNewHero(){
+        return new Hero("Arnold",23,"flying","fire");
+    }
+    public static Hero setUpNewHero1(){
+        return new Hero("Janet",30,"Night Vision","Light");
+    }
+    public static Hero setUpNewHero2(){
+        return new Hero("Jay",30,"Night Vision","Light");
+    }
 }
