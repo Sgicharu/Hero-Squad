623041955566950a0ed0b80ae3db433682804c60
Susan Gicharu <SGicharu@safaricom.co.ke>
2023-05-17 13:23:21 +0300

    updated app

--- src/main/app.java
+++ src/main/app.java
@@ -25,3 +25,28 @@ public class App {
             port = 4567;
         }
         port(port);
+
+        staticFileLocation("/public");
+        Hero.setUpNewHero();
+        Hero.setUpNewHero1();
+        Squad.setUpNewSquad();
+//        Squad.setUpNewSquad1();
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero-form",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getAllInstances();
+            model.put("hero",hero);
+            return new ModelAndView(model, "hero.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        
