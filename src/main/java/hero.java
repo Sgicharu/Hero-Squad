7aa896c35c2db70f531a6f1b6f3537f0ed26eef9
Susan Gicharu <SGicharu@safaricom.co.ke>
2023-05-17 13:45:29 +0300

    edited hero

--- src/main/java/hero.java
+++ src/main/java/hero.java
@@ -10,4 +10,25 @@ public class hero {
     private String weakness;
     private int id;
     private static ArrayList<hero> instances = new ArrayList<>();
+
+    public Hero(String name, Integer age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    public String getName() {return this.name;}
+    //    public String getCv() {
+//        String cv = this.name && this.power;
+//    }
+    public int getAge() {return this.age;}
+    public String getPower() {return this.power;}
+    public String getWeakness() {return this.weakness;}
+    public static ArrayList<hero> getAllInstances() {return instances;}
+    public static void clearAllHeroes(){instances.clear();}
+    public int getId(){return id;}
+    public static hero findById(int id) {return instances.get(id-1);}
 }
